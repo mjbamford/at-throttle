e3a609f31cf3c4575c391a592e76593383d89dfa
Michael Bamford <michael@michaelbamford.com>
2018-04-22 14:11:43 +1000

    Refactor home#throttle into a controller concern

---
+++ app/controllers/concerns/throttle.rb
@@ -0,0 +1,47 @@
+require 'digest/sha1'
+
+module Throttle
+  extend ActiveSupport::Concern
+
+  included do
+    class RateLimitError < StandardError; end
+
+    THROTTLE_WINDOW = 5 # seconds
+    THROTTLE_REQUEST_COUNT = 5
+
+    @semaphore = Mutex.new
+    @queues = {}
+
+    class << self
+      attr_reader :semaphore, :queues
+    end
+
+    before_action :throttle
+
+    rescue_from RateLimitError do
+      head :too_many_requests
+    end
+  end
+
+  def throttle
+    self.class.semaphore.synchronize do
+      time_now = Time.now.to_f
+      queues = self.class.queues
+      queue = (queues[fingerprint] ||= [])
+      queue << time_now
+      if queue.length > THROTTLE_REQUEST_COUNT
+        time_at_window_head = queue.shift
+        if (time_now - time_at_window_head) < THROTTLE_WINDOW
+          raise RateLimitError
+        end
+      end
+    end
+  end
+
+  def fingerprint
+    @fingerprint ||= begin
+      seed = request.remote_ip + request.user_agent
+      Digest::SHA1.hexdigest seed
+    end
+  end
+end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,51 +1,7 @@
-require 'thread'
-require 'digest/sha1'
-
 class HomeController < ApplicationController
-  class RateLimitError < StandardError; end
-
-  OK = 'ok'
-  THROTTLE_WINDOW = 5 # seconds
-  THROTTLE_REQUEST_COUNT = 5
-
-  @semaphore = Mutex.new
-  @queues = {}
-
-  before_action :throttle, only: :index
-
-  rescue_from RateLimitError do
-    head :too_many_requests
-  end
+  include Throttle
 
   def index
     render plain: self.class.queues.inspect
-  end
-
-  class << self
-    attr_reader :semaphore, :queues
-  end
-
-  private
-
-  def throttle
-    self.class.semaphore.synchronize do
-      time_now = Time.now.to_f
-      queues = self.class.queues
-      queue = (queues[fingerprint] ||= [])
-      queue << time_now
-      if queue.length > THROTTLE_REQUEST_COUNT
-        time_at_window_head = queue.shift
-        if (time_now - time_at_window_head) < THROTTLE_WINDOW
-          raise RateLimitError
-        end
-      end
-    end
-  end
-
-  def fingerprint
-    @fingerprint ||= begin
-      seed = request.remote_ip + request.user_agent
-      Digest::SHA1.hexdigest seed
-    end
   end
 end
